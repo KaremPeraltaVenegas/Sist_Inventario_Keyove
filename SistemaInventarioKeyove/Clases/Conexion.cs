8e0c0769c2875e3698bef156a6d74b38488d287a
Karen Peralta <karenperaltavenegas@gmail.com>
2025-10-23 16:03:30 -0500

    Clase de coneccion

---
+++ SistemaInventarioKeyove/Clases/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SistemaInventarioKeyove.Clases
+{
+    public class Conexion
+    {
+
+        private readonly string cadenaConexion =
+            ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+
+        public SqlConnection AbrirConexion()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(cadenaConexion);
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al conectar con la base de datos: " + ex.Message);
+            }
+        }
+
+        public void CerrarConexion(SqlConnection conexion)
+        {
+            if (conexion != null && conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+
+}
